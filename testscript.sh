382e75bf1758bbe86bea890d836a56af254cf45f
Tarek Ahamed <133271755+AhamedTheMorningStar@users.noreply.github.com>
2023-11-29 09:19:28 -0800

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 echo "This is an example of a script from github" 
 echo "That is running in a build step"
-NUM=$(($FIRSTNUMBER + $SECONDNUMBER))
-echo "$NUM"
 echo "Completed"
 echo "Kill them all"
